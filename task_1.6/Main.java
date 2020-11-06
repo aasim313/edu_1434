f6e9793e19e7fd0f104272ca76ed8556d42b355e
aasim313 <58509000+aasim313@users.noreply.github.com>
2020-11-06 16:10:36 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,9 +1,13 @@
 /*
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
-
+import java.util.Scanner;  // подключение класса Scanner
 public class Main {
     public static void main(String[] args) {
         // Ваш код
+        Scanner scan = new Scanner(System.in);
+        float number100 = scan.nextInt();
+        float number115 = (float) 1.15 * number100;
+        System.out.println("115 % от введенного числа: " + number115);
     }
 }
