8696bb4f68025b506e04a71d367d939d92951e58
aasim313 <58509000+aasim313@users.noreply.github.com>
2020-11-06 15:36:58 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,10 +1,11 @@
 /* Дана переменная number.
 Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
 
-public class Solution {
+public class Main {   // class Solution - > class Main
     public static int number = 25;
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.print(number*number);
     }
 }
