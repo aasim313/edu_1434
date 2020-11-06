993791ec298dff8adb57246f3df972b1c51bdfec
aasim313 <58509000+aasim313@users.noreply.github.com>
2020-11-06 16:18:34 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -8,9 +8,19 @@
 250000 */
 
 
-
+import java.util.Scanner;  // подключение класса Scanner
 public class Main {
     public static void main(String[] args) {
         //Ваш код
+        Scanner scan = new Scanner(System.in);
+        System.out.println("введите длину бассейна (м): ");
+        float a = scan.nextInt() * (float) 10.0;
+        System.out.println("введите ширину бассейна (м): ");
+        float b = scan.nextInt() * (float) 10.0;;
+        System.out.println("введите глубину бассейна (м): ");
+        float c = scan.nextInt() * (float) 10.0;;
+        float v = a*b*c;
+
+        System.out.println("Объем бассейна: " + v + "литров" );
     }
 }
