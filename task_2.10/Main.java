4d6c6dcfc9a372d0747281544b69c75d6d8208c2
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-09 16:23:19 +0300

    Update Main.java

--- task_2.10/Main.java
+++ task_2.10/Main.java
@@ -5,6 +5,16 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int arr[] = {1, 2, 3, 4, 5, 6, 1, 8, 3};
+        boolean flag = false;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) flag = true;
+            }
+        }
+            if (flag) System.out.println("да");
+            else System.out.println("нет");
+
 
     }
 }
