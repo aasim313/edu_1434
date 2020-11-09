0b77b4a3446af9c98358420a0e6749b05214be09
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-09 16:30:17 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -5,6 +5,14 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int sum = 00;
+        int i = 0;
 
+        while (sum<=10) {
+        sum+= arr[i];
+        i++;
+        }
+        System.out.println(i);
     }
 }
