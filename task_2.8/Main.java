a01da43b78476b7c34ad214fd20c2f2750dae51f
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-09 16:09:28 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -5,6 +5,12 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int arr[] = {2,3,4,5};
+        int multArr = 1;
+        for (int i = 0; i < arr.length; i++) {
+            multArr*=arr[i];
+        }
+        System.out.println("Произведение элементов массива равно: " + multArr);
 
     }
 }
