9ba6444db3190d70517abcb96aaffde99a3296ce
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-10 15:33:50 +0300

    Update Main.java

--- task_3.4/Main.java
+++ task_3.4/Main.java
@@ -7,6 +7,11 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
-
+    System.out.println(checkSum(9,2)); // проверка
     }
+    public static boolean checkSum (int a, int b) {
+        if (a+b>10 ) return true;
+        else return false;
+    }
+    
 }
