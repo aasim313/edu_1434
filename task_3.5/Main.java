1f71db3533ec7c3942fba8e941ac93dbdc51f077
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-10 16:11:19 +0300

    Update Main.java

--- task_3.5/Main.java
+++ task_3.5/Main.java
@@ -6,5 +6,15 @@
 public class Main {
     public static void main(String[] args) {
         int num[] = {4,2,6,2,65}
+        // решение
+        printArr(num, 0);
     }
+     
+    public static void printArr(int[] numbers, int i) {
+        System.out.print(numbers[i] + " " );
+            if (i < numbers.length-1) {
+                i++;
+                printArr (numbers, i);
+                }   
+    
 }
