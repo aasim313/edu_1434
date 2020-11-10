b8c17d42f5ad5eb2713cd232190d539c6a22c077
ALexey <58509000+aasim313@users.noreply.github.com>
2020-11-10 15:30:51 +0300

    Update Main.java

--- task_3.3/Main.java
+++ task_3.3/Main.java
@@ -6,6 +6,12 @@
 public class Main {
     public static void main(String[] args) {
         //напишите тут ваш код
+     System.out.println(checkEquality(2,2)); // проверка
 
     }
+    public static boolean checkEquality (int a, int b) {
+        if (a == b ) return true;
+        else return false;
+    }    
+    
 }
